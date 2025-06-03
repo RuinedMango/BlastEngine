a70370d63f88cb56cd535267634c11164e8ac382
Ruinedmango <00rudystx@gmail.com>
2025-06-02 18:09:49 -0800

    Workin

--- libs/blastpng/src/root.zig
+++ libs/blastpng/src/root.zig
@@ -37,9 +37,9 @@ const ChunkType = enum{
 var PNG_SIG = [_]u8{0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
 
 pub fn decodeFromMem(alloc: std.mem.Allocator, data: []u8, width: *u32, height: *u32, bitDepth: *u8, colorType: *u8) ![]u8{
-    const compressionMethod: u8 = 0;
-    const filterMethod: u8 = 0;
-    const interlaceMethod: u8 = 0;
+    var compressionMethod: u8 = 0;
+    var filterMethod: u8 = 0;
+    var interlaceMethod: u8 = 0;
     
     const decodedData: std.ArrayList(u8) = std.ArrayList(u8).init(alloc);
 
@@ -62,7 +62,7 @@ pub fn decodeFromMem(alloc: std.mem.Allocator, data: []u8, width: *u32, height: 
         if(ctype != null){
             try switch(ctype.?) {
                 .IHDR => handleIHDR(cdata, width, height, bitDepth, colorType, &compressionMethod, &filterMethod, &interlaceMethod),
-                .IDAT => handleIDAT(cdata, decodedData),
+                .IDAT => try handleIDAT(alloc, cdata, decodedData, width.*, height.*, colorType.*),
                 .IEND => stillReading = false,
                 else => std.debug.print("working on it\n", .{}),
             };     
@@ -94,19 +94,72 @@ fn handleIHDR(data: []u8, width: *u32, height: *u32, bitDepth: *u8, colorType: *
     interlaceMethod.* = data[12];
 }
 
-fn handleIDAT(data: []u8, out: std.ArrayList(u8)) !void{
-    var stream = std.io.fixedBufferStream(data).reader();
+fn handleIDAT(alloc: std.mem.Allocator, data: []u8, out: std.ArrayList(u8), width: u32, height: u32, colorType: u8 ) !void{
+    const stream = std.io.fixedBufferStream(data).reader();
+
+    const rowBytes: u32 = (if(colorType==3) 1 else if(colorType==2) 3 else 4) * width + 1;
+    const totalRaw = rowBytes * height;
+    var raw = try alloc.alloc(u8, totalRaw);
+    const rawWrite = std.io.fixedBufferStream(raw).writer();
+
+    std.compress.zlib.decompress(stream, rawWrite);
+
+    var prevRow = try alloc.alloc(u8, rowBytes - 1);
+    @memset(&prevRow, 0);
+    defer alloc.free(prevRow);
+
+    var readptr: usize = 0;
+    var writeptr: usize = 0;
+    for(0..height) |y| {
+        _ = y;
+
+        const filter = raw[readptr];
+        readptr += 1;
+        const rowData = raw[readptr.. readptr + (rowBytes - 1)];
+        readptr += rowBytes - 1;
+
+        var curRow = try alloc.alloc(u8, rowBytes - 1);
+        for(0..(rowBytes - 1)) |x|{
+            const rawByte = rowData[x];
+            const a = if(x > 0) curRow[x - 1] else 0;
+            const b = prevRow[x];
+            const c = if(x > 0) prevRow[x - 1] else 0;
+            curRow[x] = switch(filter){
+                0 => rawByte,
+                1 => rawByte + a,
+                2 => rawByte + b,
+                3 => rawByte + ((a + b) / 2),
+                4 => rawByte + paeth(a, b, c),
+            };
+        }
+    }
+}
+
+fn paeth(a: u8, b: u8, c: u8) u8{
+    const p = a + b - c;
+    const pa = @abs(p - a);
+    const pb = @abs(p - b);
+    const pc = @abs(p - c);
+    var pr: u8 = undefined;
+    if(pa <= pb and pa <= pc){
+        pr = a;
+    } else if(pb <= pc){
+        pr = b;
+    }else{
+        pr = c;
+    }
+    return pr;
 }
 
 test "Should work" {
-    const data = try std.f.cwd().readFileAlloc(testing.allocator, "Lenna_(test_image).png", 999999);
+    const data = try std.fs.cwd().readFileAlloc(testing.allocator, "Lenna_(test_image).png", 999999);
     defer testing.allocator.free(data);
 
     var width: u32 = 0;
     var height: u32 = 0;
     var bitDepth: u8 = 0;
     var colorType: u8 = 0;
-    const out = decodeFromMem(data, &width, &height, &bitDepth, &colorType) catch |err|{
+    const out = decodeFromMem(testing.allocator, data, &width, &height, &bitDepth, &colorType ) catch |err|{
         try testing.expect(err != LoadPNGError.InvalidPNG);
         return;
     };
